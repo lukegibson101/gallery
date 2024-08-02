b66cc42f2e42755f688a3c16c181cf8218e935ae
lukegibson101 <luke.gibson@nourishcare.com>
2024-08-02 12:50:34 +0100

    update

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,9 +1,13 @@
 <?php
 
 // Database Connection Constants
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASSWORD = '';
+const DB_NAME = 'gallery_db';
 
-define('DB_HOST','localhost');
-define('DB_USER', '');
-define('DB_PASSWORD', '');
-define('DB_NAME', '');
-define('', '');
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+if($connection) {
+    echo "We are connected";
+}
