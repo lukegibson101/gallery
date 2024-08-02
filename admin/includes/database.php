ce02db0a7316a43466ac898f478114fc44317963
lukegibson101 <luke.gibson@nourishcare.com>
2024-08-02 15:56:54 +0100

    update database connection

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -8,26 +8,29 @@ class Database {
         $this->open_db_connection();
     }
     public function open_db_connection() {
-        $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-        if(mysqli_connect_errno()) {
-            die("Database connection failed badly" . mysqli_error());
+       $this->connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+        if($this->connection->connect_errno) {
+            die("Database connection failed badly" . $this->connection->connect_error);
         }
     }
 
     public function query($sql) {
-        $result = mysqli_query($this->connection, $sql);
-
-        return $result;
+        return $this->connection->query($sql);
     }
 
     private function confirm_query($result) {
         if(!$result) {
-            die('Query Failed');
+            die('Query Failed' . $this->connection->error);
         }
     }
 
     public function escape_string($string) {
-        return mysqli_real_escape_string($this->connection, $string);
+        return $this->connection->real_escape_string($string);
+    }
+
+    public function the_insert_id() {
+        return $this->connection->insert_id;
     }
 }
 
