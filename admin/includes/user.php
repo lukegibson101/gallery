d00a0dcfbaee2bf3be79607397182189a512da77
lukegibson101 <luke.gibson@nourishcare.com>
2024-08-02 16:32:04 +0100

    user class

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -2,17 +2,21 @@
 
 class User {
 
-    public $id;
-
     public static function find_all_users() {
-        global $database;
-        return $database->query("SELECT * FROM users");
+        return self::find_this_query("SELECT * FROM users");
     }
 
     public static function find_user_by_id($id) {
         global $database;
-        $result = $database->query("SELECT * FROM users WHERE id = $id LIMIT 1");
+        $result = self::find_this_query("SELECT * FROM users WHERE id = $id LIMIT 1");
         return mysqli_fetch_array($result);
+    }
+
+    public static function find_this_query($sql) {
+        global $database;
+
+        $result_set = $database->query($sql);
+        return $result_set;
     }
 }
 
