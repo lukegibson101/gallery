6ff9b436315886bbefe7fe11b50215db384eab82
lukegibson101 <luke.gibson@nourishcare.com>
2024-08-02 15:49:28 +0100

    test database query

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -8,6 +8,14 @@
                 <small>Subheading</small>
             </h1>
 
+
+            <?php
+                $sql = "SELECT * FROM users WHERE id=1";
+                $result = $database->query($sql);
+                $user = mysqli_fetch_array($result);
+                echo $user['username'];
+            ?>
+
             <ol class="breadcrumb">
                 <li>
                     <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
