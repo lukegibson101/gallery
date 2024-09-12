a7fc8d7d71ce18dfd8b121ef80462c24a29782d8
lukegibson101 <luke.gibson@nourishcare.com>
2024-09-12 13:54:07 +0100

    add user page

--- admin/add_user.php
+++ admin/add_user.php
@@ -4,19 +4,20 @@
 
 <?php
 
-//if(isset($_POST['update'])) {
-//    if($user) {
-//        $user->title = $_POST['title'];
-//        $user->caption = $_POST['caption'];
-//        $user->alternate_text = $_POST['alternate_text'];
-//        $user->description = $_POST['description'];
-//
-//        $user->save();
-//        redirect("edit_user.php?id={$user->id}");
-//    }
-//
-//}
-//$users = user::find_all();
+$user = new User();
+
+if(isset($_POST['create'])) {
+    if($user) {
+        $user->username = $_POST['username'];
+        $user->first_name = $_POST['first_name'];
+        $user->last_name = $_POST['last_name'];
+        $user->password = $_POST['password'];
+
+        echo $user->username;
+        $user->set_file($_FILES['user_image']);
+        $user->save_user_and_image();
+    }
+}
 
 ?>
 
@@ -42,12 +43,15 @@
             <div class="row">
                 <div class="col-lg-12">
                     <h1 class="page-header">
-                        users
-                        <small>Subheading</small>
+                        Add User
                     </h1>
 
                     <form action="" method="post" enctype="multipart/form-data">
-                        <div class="col-md-8">
+                        <div class="col-md-6 col-md-offset-3">
+                            <div class="form-group">
+                                <input type="file" name="user_image">
+                            </div>
+
                             <div class="form-group">
                                 <label for="username">Username</label>
                                 <input type="text" name="username" class="form-control">
@@ -57,13 +61,19 @@
                                 <label for="first_name">First Name</label>
                                 <input type="text" name="first_name" class="form-control">
                             </div>
+
                             <div class="form-group">
                                 <label for="last_name">Last Name</label>
                                 <input type="text" name="last_name" class="form-control">
                             </div>
+
                             <div class="form-group">
                                 <label for="password">Password</label>
                                 <input type="password" name="password" class="form-control">
+                            </div>
+
+                            <div class="form-group">
+                                <input type="submit" name="create" class="btn btn-primary pull-right">
                             </div>
                         </div>
                     </form>
